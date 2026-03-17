865c021154606bbae77b9b030e7adc78330a1169
Manasi Gedam <manasivgedam2001@gmail.com>
2026-03-17 14:23:34 +0530

    Add files via upload

---
+++ sample.js
@@ -0,0 +1,8 @@
+let d = "Welcome to Node JS "
+console.log(d);
+ console.log(2 + 23);
+console.log(2+ 12);
+function add(a , f) {  return (a+f)}
+function add(a , f)  {  return (a+f)}
+console.log(add(23 , 45));
+console.log(15 * 5);
